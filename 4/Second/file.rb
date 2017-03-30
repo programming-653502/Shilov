331c344acc075a8a326da77a9ab48eb87a2f5586
storm2513 <storm25132513@gmail.com>
2017-03-30 23:14:59 +0300

    optimized file generating in the 4th lab

--- 4/Second/file.rb
+++ 4/Second/file.rb
@@ -1,10 +1,11 @@
 size = 100000
 array = []
 
+File.open('file.txt', 'w'){ |file| file.write "" }
+
 puts Time.now
-1.upto(size) do |i|
-	array << i
-end
+array = (1..size).to_a
+puts Time.now
 
 symbols_and_numbers = ['a'..'z','A'..'Z','0'..'9'].map{ |range| range.to_a }.flatten
 symbols = ['a'..'z','A'..'Z'].map{ |range| range.to_a }.flatten
@@ -13,10 +14,8 @@ string = ""
 
 size.times do |i|
 	number = array.delete_at(rand(array.size)).to_s
-	string += (0...32).map{ symbols_and_numbers[rand(symbols_and_numbers.size)] }.join + number + (0...(8 - number.size)).map{ symbols[rand(symbols.size)] }.join + "\n"
+	File.open('file.txt', 'a'){ |file| file.write (0...32).map{ symbols_and_numbers[rand(symbols_and_numbers.size)] }.join + number + (0...(8 - number.size)).map{ symbols[rand(symbols.size)] }.join + "\n" }
 end
 
-File.open('file.txt', 'w'){ |file| file.write string }
 puts "Файл записан"
-
 puts Time.now
